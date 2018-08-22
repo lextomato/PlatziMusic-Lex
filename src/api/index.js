13472101890e4e2cf16e34a43bd69da34670e551
lextomato <jhojaforce@gmail.com>
2018-08-22 00:23:43 -0430

    Fixes por la URL

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import config from './config'
 
 const { apiKey } = config
-const URL = 'http://ws.audioscrobbler.com/2.0/?method=geo.gettopartists&country=:country&api_key=ee1083bf7c4e7638cdeaddc664526d9d&format=json'
+const URL = 'https://ws.audioscrobbler.com/2.0/?method=geo.gettopartists&country=:country&api_key=ee1083bf7c4e7638cdeaddc664526d9d&format=json'
 
 export default function getArtists(country) {
   const url = URL.replace(':country', country)
